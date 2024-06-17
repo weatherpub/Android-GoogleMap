a73ada20ce7e0b0cbcde7c48f27eeb8300aad6f4
weatherpub <patrick@bayareamedia.net>
2024-06-16 17:37:57 -0700

    Project Init.

---
+++ app/src/main/java/edu/sfsu/googlemap/splash/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.sfsu.googlemap.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import edu.sfsu.googlemap.MapsActivity;
+import edu.sfsu.googlemap.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Needed for the splash screen functionality.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+
+                finish();
+            }
+        }, 5000);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
